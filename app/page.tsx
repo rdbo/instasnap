44df31ae0afe9def93b6c56a715b77033aa023a9
rdbo <rdbodev@gmail.com>
2024-05-05 13:18:22 -0300

    fixed buggy corner when scrolling on dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -11,32 +11,38 @@ export default function Home() {
       <SidePanel />
 
       <div className="grow h-screen overflow-y-scroll">
-        <div className="rounded-2xl rounded-r-none rounded-b-none bg-zinc-50 flex items-center sticky top-0 px-2 py-2">
-          <div className="w-96 flex grow">
-            <Input
-              className="rounded-full rounded-r-none h-8"
-              placeholder="Search for people, posts and #tags"
-            />
-            <Button
-              variant="outline"
-              className="rounded-full rounded-l-none border-l-0 h-8 text-zinc-500"
-            >
-              <Search size={iconSize} />
-            </Button>
-          </div>
+        <div className="sticky top-0 bg-zinc-200">
+          <div className="bg-zinc-50 flex items-center px-2 py-2 rounded-2xl rounded-b-none rounded-r-none">
+            <div className="w-96 flex grow">
+              <Input
+                className="rounded-full rounded-r-none h-8"
+                placeholder="Search for people, posts and #tags"
+              />
+              <Button
+                variant="outline"
+                className="rounded-full rounded-l-none border-l-0 h-8 text-zinc-500"
+              >
+                <Search size={iconSize} />
+              </Button>
+            </div>
 
-          <div className="flex items-center ml-2">
-            <Button variant="outline" className="rounded-full w-8 h-8 px-0 py-0">
-              <Bell size={iconSize}/>
-            </Button>
+            <div className="flex items-center ml-2">
+              <Button
+                variant="outline"
+                className="rounded-full w-8 h-8 px-0 py-0"
+              >
+                <Bell size={iconSize} />
+              </Button>
 
-            <Button variant="outline" className="rounded-full w-8 h-8 px-0 py-0 ml-1">
-              <Mail size={iconSize}/>
-            </Button>
+              <Button
+                variant="outline"
+                className="rounded-full w-8 h-8 px-0 py-0 ml-1"
+              >
+                <Mail size={iconSize} />
+              </Button>
 
-            <Button className="ml-2">
-              New Post
-            </Button>
+              <Button className="ml-2">New Post</Button>
+            </div>
           </div>
         </div>
 
