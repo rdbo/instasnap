ceb954dd6b1b3806345d9e6e2d5ace50977c7509
rdbo <rdbodev@gmail.com>
2024-05-05 12:27:36 -0300

    made side panel bigger

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <main className="flex grow">
       <div className="bg-zinc-100 px-2 py-2 relative">
         <h1 className={`${pacifico.className} text-2xl`}>InstaSnap</h1>
-        <div className="mt-8 flex flex-col items-center">
+        <div className="mt-8 flex flex-col items-center w-60">
           <div className="bg-gradient-to-r from-rose-600 to-amber-400 px-1 py-1 rounded-full">
             <div className="px-2 py-2 bg-zinc-100 rounded-full">
               <img className="rounded-full bg-gray-400 w-40 h-40" />
