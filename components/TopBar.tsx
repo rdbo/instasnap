ac4e8b03a6d47e821fac47e2cbb9924d4a6a4ca3
rdbo <rdbodev@gmail.com>
2024-05-06 08:55:59 -0300

    added search bar overlay for mobile

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,33 +1,48 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Bell, Mail, Plus, Search } from "lucide-react";
+import { Bell, X, Mail, Plus, Search } from "lucide-react";
 import AppLogo from "./AppLogo";
 import TopNavBar from "./TopNavBar";
+import { useState } from "react";
+
+const iconSize = 16;
+
+function SearchBar({ className = "" } : { className?: string }) {
+  return (
+    <div className={`ml-2 grow ${className}`}>
+      <Input
+        className="rounded-full rounded-r-none h-8 text-sm sm:text-base px-2 sm:px-3"
+        placeholder="Search for people, posts and #tags"
+      />
+      <Button
+        variant="outline"
+        className="rounded-full rounded-l-none border-l-0 h-8 text-zinc-500"
+      >
+        <Search size={iconSize} />
+      </Button>
+    </div>
+  );
+}
 
 export default function TopBar() {
-  const iconSize = 16;
+  const [showSearchOverlay, setShowSearchOverlay] = useState(false);
 
   return (
     <div className="bg-zinc-50 lg:bg-zinc-200">
-      <div className="bg-zinc-50 flex items-center justify-between md:justify-normal px-4 lg:px-8 py-2 lg:rounded-2xl lg:rounded-b-none lg:rounded-r-none">
+      <div className="bg-zinc-50 flex items-center justify-between md:justify-normal px-4 lg:px-8 py-2 lg:rounded-2xl lg:rounded-b-none lg:rounded-r-none relative">
         <div className="lg:hidden">
           <AppLogo className="text-2xl" />
         </div>
-        <div className="ml-2 w-96 hidden md:flex grow">
-          <Input
-            className="rounded-full rounded-r-none h-8"
-            placeholder="Search for people, posts and #tags"
-          />
-          <Button
-            variant="outline"
-            className="rounded-full rounded-l-none border-l-0 h-8 text-zinc-500"
-          >
-            <Search size={iconSize} />
-          </Button>
-        </div>
+        <SearchBar className="hidden md:flex"/>
 
         <div className="flex items-center ml-2">
-          <Button variant="outline" className="md:hidden rounded-full w-8 h-8 px-0 py-0">
+          <Button
+            variant="outline"
+            className="md:hidden rounded-full w-8 h-8 px-0 py-0"
+            onClick={() => setShowSearchOverlay(!showSearchOverlay)}
+          >
             <Search size={iconSize} />
           </Button>
 
@@ -50,6 +65,19 @@ export default function TopBar() {
               <Plus size={iconSize} />
             </span>
             <span className="hidden md:inline">New Post</span>
+          </Button>
+        </div>
+
+        <div
+          className={`absolute top-0 left-0 w-full bg-zinc-50 h-full ${showSearchOverlay ? "flex" : "hidden"} md:hidden items-center`}
+        >
+          <SearchBar className="flex" />
+          <Button
+            variant="destructive"
+            onClick={() => setShowSearchOverlay(false)}
+            className="rounded-full w-6 h-6 flex items-center justify-center px-0 py-0 mx-1"
+          >
+            <X size="12" />
           </Button>
         </div>
       </div>
