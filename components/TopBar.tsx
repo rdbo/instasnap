633ec7aeab309d89c88bad3bc5f83d45164140df
rdbo <rdbodev@gmail.com>
2024-05-06 08:20:30 -0300

    improved topbar for mobile

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,19 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Bell, Mail, Search } from "lucide-react";
+import { Bell, Mail, Plus, Search } from "lucide-react";
 import AppLogo from "./AppLogo";
 import TopNavBar from "./TopNavBar";
 
 export default function TopBar() {
-  const iconSize = 18;
+  const iconSize = 16;
 
   return (
     <div className="bg-zinc-50 lg:bg-zinc-200">
-      <div className="bg-zinc-50 flex items-center px-4 lg:px-8 py-2 lg:rounded-2xl lg:rounded-b-none lg:rounded-r-none">
+      <div className="bg-zinc-50 flex items-center justify-between md:justify-normal px-4 lg:px-8 py-2 lg:rounded-2xl lg:rounded-b-none lg:rounded-r-none">
         <div className="lg:hidden">
           <AppLogo className="text-2xl" />
         </div>
-        <div className="ml-2 w-96 flex grow">
+        <div className="ml-2 w-96 hidden md:flex grow">
           <Input
             className="rounded-full rounded-r-none h-8"
             placeholder="Search for people, posts and #tags"
@@ -27,7 +27,14 @@ export default function TopBar() {
         </div>
 
         <div className="flex items-center ml-2">
-          <Button variant="outline" className="rounded-full w-8 h-8 px-0 py-0">
+          <Button variant="outline" className="md:hidden rounded-full w-8 h-8 px-0 py-0">
+            <Search size={iconSize} />
+          </Button>
+
+          <Button
+            variant="outline"
+            className="rounded-full w-8 h-8 px-0 py-0 ml-1"
+          >
             <Bell size={iconSize} />
           </Button>
 
@@ -38,7 +45,12 @@ export default function TopBar() {
             <Mail size={iconSize} />
           </Button>
 
-          <Button className="ml-2">New Post</Button>
+          <Button className="ml-2">
+            <span className="md:hidden">
+              <Plus size={iconSize} />
+            </span>
+            <span className="hidden md:inline">New Post</span>
+          </Button>
         </div>
       </div>
       <div className="lg:hidden">
