638f86984a63ad3e37bdd0fa274f604ea083e4e4
rdbo <rdbodev@gmail.com>
2024-05-07 12:34:16 -0300

    added server-side loading of posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,42 @@
 import BottomNavBar from "@/components/BottomNavBar";
 import SidePanel from "@/components/SidePanel";
 import TopBar from "@/components/TopBar";
+import { Suspense } from "react";
 
-export default function Home() {
+async function getPosts() {
+  "use server";
+
+  await new Promise((r) => setTimeout(r, 500));
+
+  return ["hello", "hey there", "wassup"];
+}
+
+async function Posts() {
+  const posts = await getPosts();
+
+  return (
+    <>
+      {posts.map((post, index) => (
+        <h1 key={index} className="text-4xl">{post}</h1>
+      ))}
+    </>
+  );
+}
+
+export default async function Home() {
   return (
     <main className="flex grow bg-zinc-200">
       <SidePanel />
 
-      <div className="grow h-screen overflow-y-scroll lg:rounded-b-2xl lg:rounded-r-none">
+      <div className="grow h-screen overflow-y-scroll lg:rounded-b-2xl lg:rounded-r-none bg-zinc-50">
         <div className="sticky top-0">
           <TopBar />
         </div>
 
-        <div className="bg-zinc-50 px-8 pb-20">
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
-          <h1 className="text-8xl">hello</h1>
+        <div className="px-8 pb-20">
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Posts />
+          </Suspense>
         </div>
 
         <div className="fixed bottom-0 lg:hidden bg-zinc-50 h-14 flex justify-center items-center w-full">
