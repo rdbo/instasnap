4b50290852d1ca2cdc325310a9f1b803fce1eba3
rdbo <rdbodev@gmail.com>
2024-05-09 13:11:49 -0300

    improved posts

--- components/Post.tsx
+++ components/Post.tsx
@@ -10,13 +10,10 @@ export interface PostProps {
 function shortenNumber(n: number): string {
   const convertionTable = ["K", "M", "B", "T"];
 
-  console.log(n);
-
   for (var i = -1; i < convertionTable.length - 1; ++i) {
     if (n < 1000) break;
 
     n /= 1000;
-    console.log(n);
   }
 
   let converted = n.toFixed(1) + convertionTable[i];
@@ -32,9 +29,11 @@ export default function Post({ media, text, likes, comments }: PostProps) {
   return (
     <div className="brightness-90 w-80 sm:w-96">
       <div className="w-full flex flex-col justify-center items-center">
-        <img src={media[0].source} className="max-w-80 sm:max-w-96 max-h-96" />
-        <p className="h-16">{text}</p>
+        <img src={media[0].source} className="w-full max-h-96" />
       </div>
+
+      <p className="h-16">{text}</p>
+
       <div className="flex justify-between mt-2">
         <div className="flex items-center text-gray-200">
           <MessageCircle size={iconSize} fill={iconFill} color={iconColor} />
