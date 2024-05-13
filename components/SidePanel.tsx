2dd2d10accba0abaf4aba892e9f19550398a5db8
rdbo <rdbodev@gmail.com>
2024-05-13 09:42:40 -0300

    fixed side panel colors

--- components/SidePanel.tsx
+++ components/SidePanel.tsx
@@ -8,8 +8,8 @@ export default function SidePanel() {
       <AppLogo className="text-2xl" />
       <div className="mt-8 flex flex-col items-center w-60">
         <div className="bg-gradient-to-r from-rose-600 to-amber-400 px-1 py-1 rounded-full">
-          <div className="px-2 py-2 bg-zinc-200 rounded-full">
-            <img className="rounded-full bg-gray-400 w-40 h-40" />
+          <div className="px-2 py-2 bg-zinc-100 rounded-full">
+            <img className="rounded-full bg-gray-300 w-40 h-40" />
           </div>
         </div>
 
