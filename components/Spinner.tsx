de29fbe0f18b2f8b935f3958c8667fb6f2d7e56e
rdbo <rdbodev@gmail.com>
2024-05-16 08:54:34 -0300

    added sign out page

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,5 +1,5 @@
 import { LoaderCircle, LucideProps } from "lucide-react";
 
-export default function Spinner({ ...props }: LucideProps) {
-  return <LoaderCircle className="animate-spin" {...props} />;
+export default function Spinner({ className, ...props }: LucideProps) {
+  return <LoaderCircle className={`animate-spin ${className}`} {...props} />;
 }
